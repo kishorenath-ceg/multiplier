32f677987ba06cfd6a85372906fc542c306f1d20
kishorenath-ceg <saravanankishor679@gmail.com>
2026-07-15 15:02:17 +0530

    name added

--- Main.java
+++ Main.java
@@ -5,8 +5,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-          System.out.println("hi kiss");
         System.out.println(multiply(5, 4));
+        System.out.println("hi kishore");
+        System.out.println("hi vibeesh");
     }
 }
 
